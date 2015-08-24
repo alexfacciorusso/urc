f57765f9a2b1a50e17695686e1a2a19663338815
Alex Facciorusso <alexfacciorusso93@gmail.com>
2015-08-24 14:03:48 +0200

    Renamed urcsample to sample.

--- sample/src/main/java/com/alexfacciorusso/urc/sample/MainActivity.java
+++ sample/src/main/java/com/alexfacciorusso/urc/sample/MainActivity.java
@@ -1,9 +1,12 @@
 package com.alexfacciorusso.urc.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import com.alexfacciorusso.urc.Urc;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,27 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        String result = Urc.with("http://mysite.com")
+                .fromEndpoint("something/:id/page/:pageNumber")
+                .addParameter("id", 2)
+                .addParameter("pageNumber", 1)
+                .build();
+
+        TextView resultTextView = (TextView) findViewById(R.id.results);
+        resultTextView.setText(result);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
